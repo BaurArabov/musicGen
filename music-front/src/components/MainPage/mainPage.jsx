4dbf604f432bd9fdac1506bf2493bdc274767a45
Baur Arabov <baur2003arabov@gmail.com>
2023-03-12 04:33:34 +0600

    add player

--- music-front/src/components/MainPage/mainPage.jsx
+++ music-front/src/components/MainPage/mainPage.jsx
@@ -4,29 +4,73 @@ import Search from "../../assets/white-magnifying-glass-icon-png-14.jpg";
 import axios from "axios";
 import useAuth from "../useAuth/useAuth";
 import SpotifyWebApi from "spotify-web-api-node";
+import { List, ListItem, ListItemButton, ListItemText } from "@mui/material";
+import Player from "../Player/Player";
 
-// const spotifyApi = new SpotifyWebApi({
-//   clientId: "33665286bad84fc388bdc384fc451680",
-// });
+const spotifyApi = new SpotifyWebApi({
+  clientId: "33665286bad84fc388bdc384fc451680",
+});
 
 function Main(props) {
   const accessToken = useAuth(props.code);
+
   const [songs, setSongs] = useState("");
   const [inputValue, setInputValue] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [uri, setUri] = useState([]);
+  const [currentSongUri, setCurrentSongUri] = useState("");
 
   const handleChangeInput = (event) => {
-    console.log(event.target.value);
     setInputValue(event.target.value);
   };
 
   const handleKeyDown = async (event) => {
-    console.log(songs);
     if (inputValue === "") return;
     if (event.key === "Enter") {
       const response = await axios.post("http://localhost:8080/songs", {
         mood: inputValue,
       });
-      setSongs(response.data.songs);
+
+      const songs = response.data.songs.map((song) => ({
+        id: song.id,
+        name: song.songName,
+        artist: song.artist,
+        uri: "",
+      }));
+
+      setSongs(songs);
+
+      try {
+        const promises = songs.map((song) =>
+          axios.get(
+            `https://api.spotify.com/v1/search?q=track:${song.name} artist:${song.artist}&type=track`,
+            {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+              },
+            }
+          )
+        );
+        const responses = await Promise.all(promises);
+
+        const uris = responses
+          .map((response, index) => {
+            const uri = response.data.tracks.items[0]?.uri;
+            if (uri) {
+              songs[index].uri = uri;
+            }
+            return uri;
+          })
+          .filter(Boolean);
+
+        setUri(uris);
+        setSearchResults(
+          responses.map((response) => response.data.tracks.items)
+        );
+        setSongs(songs);
+      } catch (error) {
+        console.error(error);
+      }
     }
   };
 
@@ -34,10 +78,14 @@ function Main(props) {
     setInputValue("");
   };
 
-  // useEffect(() => {
-  //   if (!accessToken) return;
-  //   spotifyApi.setAccessToken(accessToken);
-  // }, [accessToken]);
+  useEffect(() => {
+    if (!accessToken) return;
+    spotifyApi.setAccessToken(accessToken);
+  }, [accessToken]);
+
+  const handleCurrSong = (uri) => {
+    setCurrentSongUri(uri);
+  };
 
   return (
     <div className="main-content">
@@ -61,116 +109,55 @@ function Main(props) {
           <button>Radio</button>
         </div>
       </div>
-      {songs !== "" && (
+      {songs !== "" ? (
         <div className="content">
-          <h2>Related to your mood: {inputValue}</h2>
-          <ul>
+          <div style={{ display: "flex", gap: "10px" }}>
+            <h2 style={{ color: "white" }}>Related to your mood: </h2>{" "}
+            <h2 style={{ color: "red" }}>{inputValue}</h2>
+          </div>
+
+          <List
+            className="list-songs"
+            style={{ overflowY: "scroll", height: "430px" }}
+          >
             {songs.map((song) => (
-              <li key={song.id}>
-                {song.songName} - {song.artist}
-              </li>
+              <ListItem key={song.id} disablePadding>
+                <ListItemButton onClick={() => handleCurrSong(song.uri)}>
+                  {song.uri !== "" ? (
+                    <ListItemText
+                      primary={`${song.name} - ${song.artist}.`}
+                      style={{ color: "white" }}
+                    />
+                  ) : (
+                    <ListItemText
+                      primary={`${song.name} - ${song.artist}`}
+                      style={{ color: "white" }}
+                    />
+                  )}
+                </ListItemButton>
+              </ListItem>
             ))}
-          </ul>
+          </List>
         </div>
+      ) : songs === undefined ? (
+        <h1>Try again...</h1>
+      ) : (
+        <></>
       )}
+      <div className="spot-player">
+        <Player
+          accessToken={accessToken}
+          trackUri={currentSongUri}
+          style={{
+            sliderColor: "red",
+            trackNameColor: "black",
+            trackArtistColor: "black",
+          }}
+          songs={songs}
+        />
+      </div>
     </div>
   );
 }
 
 export default Main;
-
-// import React, { useState, useEffect } from "react";
-// import "./mainPage.css";
-// import Search from "../../assets/white-magnifying-glass-icon-png-14.jpg";
-// import useAuth from "../useAuth/useAuth";
-// import SpotifyWebApi from "spotify-web-api-node";
-// import { PlayCircle } from "react-feather";
-// import axios from "axios";
-
-// const spotifyApi = new SpotifyWebApi({
-//   clientId: "33665286bad84fc388bdc384fc451680",
-// });
-
-// function Main(props) {
-//   const accessToken = useAuth(props.code);
-//   const [inputValue, setInputValue] = useState("");
-//   const [searchResults, setSearchResults] = useState([]);
-//   const [songs, setSongs] = useState("");
-
-//   const handleChangeInput = (event) => {
-//     setInputValue(event.target.value);
-//   };
-
-//   const handleKeyDown = async (event) => {
-//     if (inputValue === "") return;
-//     if (event.key === "Enter") {
-//       try {
-//         const response = await axios.post("http://localhost:8080/songs", {
-//           mood: inputValue,
-//         });
-//         setSongs(response.data.songs);
-//         const resResponse = await response.data.songs.map((song) => {
-//           spotifyApi.searchTracks(song, { limit: 1 });
-//         });
-//         setSearchResults(resResponse.body.tracks.items);
-//       } catch (error) {
-//         console.error(error);
-//       }
-//     }
-//   };
-
-//   const handleClear = () => {
-//     setInputValue("");
-//     setSearchResults([]);
-//   };
-
-//   const handlePlaySong = (uri) => {
-//     spotifyApi.play({ uris: [uri] }).catch((error) => console.error(error));
-//   };
-
-//   useEffect(() => {
-//     if (!accessToken) return;
-//     spotifyApi.setAccessToken(accessToken);
-//   }, [accessToken]);
-
-//   return (
-//     <div className="main-content">
-//       <div className="top-bar">
-//         <div className="mood-input">
-//           <img src={Search} alt="" />
-//           <input
-//             type="text"
-//             placeholder="Write your mood"
-//             value={inputValue}
-//             onChange={handleChangeInput}
-//             onKeyDown={handleKeyDown}
-//           />
-//           <span className="close" onClick={handleClear}>
-//             &times;
-//           </span>
-//         </div>
-//         <div className="buttons">
-//           <button>Home</button>
-//           <button>Browse</button>
-//           <button>Radio</button>
-//         </div>
-//       </div>
-//       {searchResults.length > 0 && (
-//         <div className="content">
-//           <h2>Related to your mood: {inputValue}</h2>
-//           <ul>
-//             {searchResults.map((result) => (
-//               <li key={result.id}>
-//                 {result.name} -{" "}
-//                 {result.artists.map((artist) => artist.name).join(", ")}
-//                 <PlayCircle onClick={() => handlePlaySong(result.uri)} />
-//               </li>
-//             ))}
-//           </ul>
-//         </div>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default Main;
--- music-front/src/components/Player/player.jsx
+++ music-front/src/components/Player/player.jsx
@@ -1,5 +1,43 @@
-import "./index.css";
+import React, { useState, useEffect } from "react";
+import SpotifyPlayer from "react-spotify-web-playback";
 
-export default function Player() {
-  return <div>Player</div>;
+function Player({ accessToken, trackUri, styles }) {
+  const [play, setPlay] = useState(false);
+
+  useEffect(() => {
+    setPlay(true);
+  }, [trackUri]);
+
+  const handlePlay = () => {
+    setPlay(!play);
+  };
+
+  const handleEnd = () => {
+    setPlay(false);
+  };
+
+  return (
+    <div>
+      <SpotifyPlayer
+        token={accessToken}
+        uris={trackUri ? [trackUri] : "spotify:track:6lEBtO2UwVILU8B0H23msN"}
+        play={play}
+        callback={(state) => {
+          if (!state.isPlaying) setPlay(false);
+        }}
+        styles={styles}
+        showSaveIcon
+        showPlayIcon
+        autoPlay
+        magnifySliderOnHover
+        playIcon={<span>&#9654;</span>}
+        pauseIcon={<span>&#10074;&#10074;</span>}
+        onPlay={() => handlePlay()}
+        onReady={() => handlePlayerReady()}
+        onEnd={() => handleEnd()}
+      />
+    </div>
+  );
 }
+
+export default Player;
